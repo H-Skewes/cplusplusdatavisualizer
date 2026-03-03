e3a281d2ded11dcf9941f487ec896c68c1d5394b
Henry Skewes <info@henryskewes.com>
2026-03-02 21:26:31 -0600

    added new size function and preparing set of tuple return from findsetofzeros

--- mathhelpers.cpp
+++ mathhelpers.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <tuple>
 #include "mathhelpers.h"
+
+
 std::vector<std::vector<int>> buildmatrix(int rows, int columns){
     std::vector<std::vector<int>> entirematrix(rows, std::vector<int>(columns, 0));
     std::cout<<"Enter values left to right --"<<std::endl;
@@ -8,7 +12,7 @@ std::vector<std::vector<int>> buildmatrix(int rows, int columns){
     for (int rownum = 0; rownum < rows; rownum++){
 		for (int colnum =0; colnum < columns; colnum++){
 			int inputval;
-            std::cout<<"Row "<<rownum+1<<" Column "<<colnum+1<<" value:";
+            std::cout<<"Row "<<rownum+1<<" Column "<<colnum+1<<" Value:";
             std::cin >> inputval;
             entirematrix[rownum][colnum] = inputval;
 		}
@@ -17,9 +21,34 @@ std::vector<std::vector<int>> buildmatrix(int rows, int columns){
     return entirematrix;
 }
 
+
+std::tuple<int, int> getsizeofmatrix(std::vector<std::vector<int>> entirematrix){
+    size_t rowsize = entirematrix.size();
+    size_t colsize = entirematrix[0].size();
+    return std::make_pair(rowsize, colsize);
+}
+
+
 std::vector<std::vector<int>> rowreductionref(std::vector<std::vector<int>> entirematrix, bool augbool){
+    int rowsize, colsize;
     std::vector<std::vector<int>> currmatrix = entirematrix;
-    size_t rowsize = currmatrix.size();
-    size_t colsize = currmatrix[0].size();
+    std::tie(rowsize, colsize) = getsizeofmatrix(entirematrix);
+    
     return currmatrix;
 }
+
+
+std::set<std::tuple<int, int>> findsetofzeros(std::vector<std::vector<int>> entirematrix){
+    int rowsize, colsize;
+    std::tie(rowsize, colsize) = getsizeofmatrix(entirematrix);
+    std::set<std::tuple<int, int>> setofzeros;
+    //for (int rownum = 0; rownum < rowsize; rownum++){
+		//for (int colnum =0; colnum < colsize; colnum++){
+        //    entirematrix[rownum][colnum] = inputval;
+		//}
+
+	//}
+    setofzeros.insert(std::make_tuple(1, 1));
+    return setofzeros;
+}
+
--- mathhelpers.h
+++ mathhelpers.h
@@ -1,8 +1,12 @@
 #ifndef MATHHELPERS_H
 #define MATHHELPERS_H
 #include <vector>
+#include <set>
+#include <tuple>
 
 std::vector<std::vector<int>> buildmatrix(int rows, int columns);
 std::vector<std::vector<int>> rowreductionref(std::vector<std::vector<int>> entirematrix, bool augbool);
-
+// this has uses in other methods later to solve for cofactor expansion and checking for zeros where needed
+std::set<std::tuple<int, int>> findsetofzeros(std::vector<std::vector<int>> entirematrix);
+std::tuple<int, int> getsizeofmatrix(std::vector<std::vector<int>> entirematrix);
 #endif // MATHHELPERS_H
